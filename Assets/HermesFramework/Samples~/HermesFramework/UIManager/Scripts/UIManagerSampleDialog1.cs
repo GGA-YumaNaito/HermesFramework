56b5382fa97901bd4f7606440a990055d273cc53
GGA-YumaNaito <y_naito@gravityga.jp>
2023-02-20 11:16:13 +0900

    [Framework]Sampleが対象に入っていなかったので追加<File>

---
+++ Assets/HermesFramework/Samples~/HermesFramework/UIManager/Scripts/UIManagerSampleDialog1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Hermes.UI.Sample
+{
+    public class UIManagerSampleDialog1 : Dialog
+    {
+        public override bool IsBack { get; protected set; } = true;
+
+        /// <summary>オプション</summary>
+        public class Options
+        {
+            public string sumpleText;
+        }
+        public override void OnLoad(object options)
+        {
+            base.OnLoad(options);
+            var op = options as Options;
+            if (op != null)
+            {
+                Debug.Log(op.sumpleText);
+            }
+        }
+
+        public async void OnClickSampleDialog2()
+        {
+            await UIManager.Instance.LoadAsync<UIManagerSampleDialog2>(new UIManagerSampleDialog2.Options() { sumpleText = "UIManagerSampleDialog2" });
+        }
+    }
+}
