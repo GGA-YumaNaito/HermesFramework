556d305f3958d84a9f248ab2e3458a5326c55f45
GGA-YumaNaito <y_naito@gravityga.jp>
2023-03-10 17:24:33 +0900

    [Framework]ViewBase修正<Script>

--- Assets/HermesFramework/UIManager/Scripts/ViewBase.cs
+++ Assets/HermesFramework/UIManager/Scripts/ViewBase.cs
@@ -92,17 +92,20 @@ namespace Hermes.UI
         {
             SetStatus(eStatus.Enable);
 
-            if (loadAnimParam.ease != Ease.Unset)
+            if (targetTransform)
             {
-                targetTransform.localPosition = loadAnimParam.targetPos;
-                targetTransform.DOLocalMove(Vector3.zero, loadAnimParam.moveTime).SetEase(loadAnimParam.ease).OnComplete(DoStatusDisplay);
-                return;
-            }
-            else if (loadAnimParam.animationCurve != null)
-            {
-                targetTransform.localPosition = loadAnimParam.targetPos;
-                targetTransform.DOLocalMove(Vector3.zero, loadAnimParam.moveTime).SetEase(loadAnimParam.animationCurve).OnComplete(DoStatusDisplay);
-                return;
+                if (loadAnimParam.ease != Ease.Unset)
+                {
+                    targetTransform.localPosition = loadAnimParam.targetPos;
+                    targetTransform.DOLocalMove(Vector3.zero, loadAnimParam.moveTime).SetEase(loadAnimParam.ease).OnComplete(DoStatusDisplay);
+                    return;
+                }
+                else if (loadAnimParam.animationCurve != null)
+                {
+                    targetTransform.localPosition = loadAnimParam.targetPos;
+                    targetTransform.DOLocalMove(Vector3.zero, loadAnimParam.moveTime).SetEase(loadAnimParam.animationCurve).OnComplete(DoStatusDisplay);
+                    return;
+                }
             }
 
             DoStatusDisplay();
@@ -117,15 +120,18 @@ namespace Hermes.UI
         {
             SetStatus(eStatus.Disable);
 
-            if (unloadAnimParam.ease != Ease.Unset)
+            if (targetTransform)
             {
-                targetTransform.DOLocalMove(unloadAnimParam.targetPos, unloadAnimParam.moveTime).SetEase(unloadAnimParam.ease).OnComplete(DoStatusEnd);
-                return;
-            }
-            else if (unloadAnimParam.animationCurve != null)
-            {
-                targetTransform.DOLocalMove(unloadAnimParam.targetPos, unloadAnimParam.moveTime).SetEase(unloadAnimParam.animationCurve).OnComplete(DoStatusEnd);
-                return;
+                if (unloadAnimParam.ease != Ease.Unset)
+                {
+                    targetTransform.DOLocalMove(unloadAnimParam.targetPos, unloadAnimParam.moveTime).SetEase(unloadAnimParam.ease).OnComplete(DoStatusEnd);
+                    return;
+                }
+                else if (unloadAnimParam.animationCurve != null)
+                {
+                    targetTransform.DOLocalMove(unloadAnimParam.targetPos, unloadAnimParam.moveTime).SetEase(unloadAnimParam.animationCurve).OnComplete(DoStatusEnd);
+                    return;
+                }
             }
 
             DoStatusEnd();
