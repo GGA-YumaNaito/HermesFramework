e62ea0fd6ffda9b694483825daf24fca3c90cf9f
GGA-YumaNaito <y_naito@gravityga.jp>
2023-05-12 17:56:56 +0900

    [Framework]PrefsManager修正<Script>

--- Assets/HermesFramework/PrefsManager/Scripts/PrefsManager.cs
+++ Assets/HermesFramework/PrefsManager/Scripts/PrefsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Hermes.Prefs
@@ -15,6 +16,9 @@ namespace Hermes.Prefs
 
         /// <summary>
         /// 値があったら一時保存
+        /// <para>valueのtypeは値型だとint,bool,enum,float,stringが使えます</para>
+        /// <para>それ以外ではJsonUtilityを用いるため、classを使用して下さい</para>
+        /// <para>classの変数はシリアライズ可能な形にして下さい(public、SerializeFieldアトリビュート等)</para>
         /// </summary>
         /// <param name="key">Key</param>
         /// <param name="value">Value</param>
@@ -22,20 +26,39 @@ namespace Hermes.Prefs
         {
             if (value == null)
                 return;
-            var json = JsonUtility.ToJson(value);
-            PlayerPrefs.SetString(key, json);
+            var type = value.GetType();
+            if (type == typeof(int) || type.IsEnum)
+                PlayerPrefs.SetInt(key, (int)value);
+            else if (type == typeof(bool))
+                PlayerPrefs.SetInt(key, Convert.ToInt32((bool)value));
+            else if (type == typeof(float))
+                PlayerPrefs.SetFloat(key, (float)value);
+            else if (type == typeof(string))
+                PlayerPrefs.SetString(key, (string)value);
+            else
+                PlayerPrefs.SetString(key, JsonUtility.ToJson(value));
         }
 
         /// <summary>
         /// 存在していたら取得、無かったらnull
+        /// <para>Tには値型だとint,bool,enum,float,stringが使えます</para>
+        /// <para>それ以外ではJsonUtilityを用いるため、classを使用して下さい</para>
         /// </summary>
         /// <typeparam name="T">T</typeparam>
         /// <param name="key">Key</param>
         /// <returns>Value</returns>
         public T Load<T>(string key)
         {
-            var json = PlayerPrefs.GetString(key, string.Empty);
-            return JsonUtility.FromJson<T>(json);
+            var type = typeof(T);
+            if (type == typeof(int) || type.IsEnum)
+                return (T)(object)PlayerPrefs.GetInt(key, 0);
+            else if (type == typeof(bool))
+                return (T)(object)Convert.ToBoolean(PlayerPrefs.GetInt(key, 0));
+            else if (type == typeof(float))
+                return (T)(object)PlayerPrefs.GetFloat(key, 0f);
+            else if (type == typeof(string))
+                return (T)(object)PlayerPrefs.GetString(key, string.Empty);
+            return JsonUtility.FromJson<T>(PlayerPrefs.GetString(key, string.Empty));
         }
 
         /// <summary>
