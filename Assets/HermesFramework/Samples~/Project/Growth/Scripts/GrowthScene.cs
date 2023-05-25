daf7ec7979af2ac33fb74eb4e25a3bd4769562ee
GGA-YumaNaito <y_naito@gravityga.jp>
2023-05-25 10:48:46 +0900

    [Framework]GrowthScene追加<File>

---
+++ Assets/HermesFramework/Samples~/Project/Growth/Scripts/GrowthScene.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Growth
+{
+    /// <summary>
+    /// Growth Scene
+    /// </summary>
+    public class GrowthScene : Hermes.UI.Screen
+    {
+        public override bool IsBack { get; protected set; } = true;
+
+        /// <summary>オプション</summary>
+        public class Options
+        {
+        }
+
+        /// <summary>
+        /// ロード
+        /// </summary>
+        /// <param name="options"></param>
+        public override UniTask OnLoad(object options)
+        {
+            return UniTask.CompletedTask;
+        }
+    }
+}
