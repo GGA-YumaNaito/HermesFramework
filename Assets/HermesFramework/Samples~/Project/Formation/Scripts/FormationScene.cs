1870f647feffad31ad135deada6287bb59c32ed4
GGA-YumaNaito <y_naito@gravityga.jp>
2023-05-25 10:48:14 +0900

    [Framework]FormationScene追加<File>

---
+++ Assets/HermesFramework/Samples~/Project/Formation/Scripts/FormationScene.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Formation
+{
+    /// <summary>
+    /// Formation Scene
+    /// </summary>
+    public class FormationScene : Hermes.UI.Screen
+    {
+        public override bool IsBack { get; protected set; } = true;
+
+        /// <summary>オプション</summary>
+        public class Options
+        {
+        }
+
+        /// <summary>
+        /// ロード
+        /// </summary>
+        /// <param name="options"></param>
+        public override UniTask OnLoad(object options)
+        {
+            return UniTask.CompletedTask;
+        }
+    }
+}
