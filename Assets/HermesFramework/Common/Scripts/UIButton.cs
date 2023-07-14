f5281070d7894fd8bf2bec3b78709189350b31cf
GGA-YumaNaito <y_naito@gravityga.jp>
2023-07-14 11:41:42 +0900

    [Framework]UIButton機能追加(長押し)<Script>

--- Assets/HermesFramework/Common/Scripts/UIButton.cs
+++ Assets/HermesFramework/Common/Scripts/UIButton.cs
@@ -2,6 +2,9 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Hermes.Sound;
+using System;
+using UnityEngine.Events;
+using UniRx;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -15,6 +18,14 @@ namespace Hermes.UI
     {
         /// <summary>SE Name</summary>
         public string seName = "";
+        /// <summary>長押しするか</summary>
+        public bool isLongPress = false;
+        /// <summary>長押しの時間</summary>
+        public float longPressDuration = 0.5f;
+        /// <summary>長押しの押下時のUnityEvent</summary>
+        public UnityEvent OnLongPressDown = new();
+        /// <summary>長押しの離した時のUnityEvent</summary>
+        public UnityEvent OnLongPressUp = new();
         /// <summary>ドラッグするか</summary>
         public bool isDrag = false;
         /// <summary>ドラッグ前の位置に戻すフラグ : true 戻す, false 戻さない</summary>
@@ -36,6 +47,15 @@ namespace Hermes.UI
         /// <summary>親のRectTransform</summary>
         RectTransform parentRectTransform;
 
+        /// <summary>長押し判定</summary>
+        bool longPressed;
+        /// <summary>オブジェクト上での長押し判定</summary>
+        bool isLongPressed;
+        /// <summary>長押し時間</summary>
+        float longPressedTime;
+        /// <summary>長押しのDisposable</summary>
+        IDisposable longPressDisposable;
+
         protected override void Awake()
         {
             base.Awake();
@@ -51,10 +71,86 @@ namespace Hermes.UI
         public override void OnPointerClick(PointerEventData eventData)
         {
             base.OnPointerClick(eventData);
+            
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                // SE呼び出し
+                if (!seName.IsNullOrEmpty())
+                    SoundManager.SE.Play(seName);
+
+                // 長押し
+                if (isLongPressed)
+                {
+                    OnLongPressUp.Invoke();
+                    isLongPressed = false;
+                }
+            }
+        }
+
+        public override void OnSubmit(BaseEventData eventData)
+        {
+            base.OnSubmit(eventData);
 
             // SE呼び出し
             if (!seName.IsNullOrEmpty())
                 SoundManager.SE.Play(seName);
+
+            // 長押し
+            if (isLongPressed)
+            {
+                OnLongPressUp.Invoke();
+                isLongPressed = false;
+            }
+        }
+
+        /// <summary>
+        /// ボタンを押下した時
+        /// </summary>
+        /// <param name="eventData">eventData</param>
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+
+            if (!isLongPress)
+                return;
+
+            longPressed = true;
+            isLongPressed = false;
+
+            if (longPressDisposable == null)
+            {
+                longPressDisposable = Observable.EveryUpdate().Subscribe(i =>
+                {
+                    if (longPressed)
+                    {
+                        longPressedTime += Time.deltaTime;
+
+                        if (longPressedTime >= longPressDuration)
+                        {
+                            longPressed = false;
+                            OnLongPressDown.Invoke();
+                            isLongPressed = true;
+                        }
+                    }
+                }).AddTo(this);
+            }
+        }
+
+        /// <summary>
+        /// ボタンを離した時
+        /// </summary>
+        /// <param name="eventData">eventData</param>
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+
+            if (!isLongPress)
+                return;
+
+            longPressed = false;
+            longPressedTime = 0f;
+            longPressDisposable.Dispose();
+            longPressDisposable = null;
         }
 
         /// <summary>
@@ -130,11 +226,25 @@ namespace Hermes.UI
             var component = (UIButton)target;
 
             EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.seName)), new GUIContent("SE Name"));
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.isLongPress)), new GUIContent("Is Long Press"));
+            if (component.isLongPress)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.longPressDuration)), new GUIContent("Long Press Duration"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.OnLongPressDown)), new GUIContent("On Long Press Down"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.OnLongPressUp)), new GUIContent("On Long Press Up"));
+                EditorGUI.indentLevel--;
+            }
+
             EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.isDrag)), new GUIContent("Is Drag"));
             if (component.isDrag)
             {
+                EditorGUI.indentLevel++;
                 EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(component.isPrevPosition)), new GUIContent("Is Prev Position"));
+                EditorGUI.indentLevel--;
             }
+
             serializedObject.ApplyModifiedProperties();
         }
     }
