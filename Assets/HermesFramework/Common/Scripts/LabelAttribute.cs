8d670cc08cf0b6bb318f9de4990e0c9be11be356
GGA-YumaNaito <y_naito@gravityga.jp>
2023-12-13 18:23:26 +0900

    [Common]LabelAttribute.cs & LabeledEnum.cs作成<Script>

---
+++ Assets/HermesFramework/Common/Scripts/LabelAttribute.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Inspectorの変数名を変更出来るようにする
+/// </summary>
+public class LabelAttribute : PropertyAttribute
+{
+    public readonly string Label;
+
+    public LabelAttribute(string label)
+    {
+        Label = label;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(LabelAttribute))]
+public class LabelAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var newLabel = attribute as LabelAttribute;
+        label.text = newLabel.Label;
+        EditorGUI.PropertyField(position, property, label, true);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, true);
+    }
+}
+#endif
---
+++ Assets/HermesFramework/Common/Scripts/LabeledEnum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/// <summary>
+/// 列挙型のフィールドにラベル文字列を付加するカスタム属性
+/// </summary>
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+public class LabeledEnum : Attribute
+{
+
+    /// <summary>ラベル文字列.</summary>
+    public string Label { get; private set; }
+
+    /// <summary>
+    /// LabeledEnumAttribute クラスの新しいインスタンスを初期化
+    /// </summary>
+    /// <param name="label">ラベル文字列.</param>
+    public LabeledEnum(string label)
+    {
+        Label = label;
+    }
+}
+
+public static class LabeledEnumExt
+{
+    /// <summary>
+    /// 属性で指定されたラベル文字列を取得
+    /// </summary>
+    /// <param name="value">ラベル付きフィールド</param>
+    /// <returns>ラベル文字列</returns>
+    public static string GetLabel(this Enum value)
+    {
+        //EnumのTypeを取得
+        // type = {Name = "フォーマット形式共通" FullName = "共通.フォーマット形式共通"}
+        Type type = value.GetType();
+
+        //Enumのフィールドを取得
+        // name = "Default"
+        string name = Enum.GetName(type, value);
+
+        //クラスの配列を取得
+        // attributes = {共通.LabeledEnum[1]}
+        if (type.GetField(name).GetCustomAttributes(typeof(LabeledEnum), false) is LabeledEnum[] attributes && attributes.Length > 0)
+        {
+            return attributes[0].Label;
+        }
+        else
+        {
+            return name;
+        }
+    }
+}
